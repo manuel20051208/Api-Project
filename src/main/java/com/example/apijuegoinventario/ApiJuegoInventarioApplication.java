fa4d475845bf916dbbd0371dda45f55d62cc9739
Manuel Ramos <manuelramos09544@gmail.com>
2026-03-21 10:20:30 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/example/apijuegoinventario/ApiJuegoInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.apijuegoinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJuegoInventarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiJuegoInventarioApplication.class, args);
+    }
+
+}
